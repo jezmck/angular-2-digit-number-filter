dd59a5c62fabd8113f843a46fcbb3e8264077dfc
Jez McKean <jezmck@users.noreply.github.com>
2015-12-08 12:16:36 +0000

    Update 2digitnumber.js

--- 2digitnumber.js
+++ 2digitnumber.js
@@ -4,7 +4,7 @@ angular.module('filter.2digitnumber', [])
         return function (num) {
             var digits = 2;
             if (num > 10) digits = 0;
-            elseif (num > 1) digits = 1;
+            else if (num > 1) digits = 1;
             return num.toFixed(digits);
         }
     });
