0bce991cf2cadc896afcd24032b77e3cf7e167a9
Jez McKean <jezmck@users.noreply.github.com>
2015-12-08 12:45:08 +0000

    Update twodigitnumber.js

--- twodigitnumber.js
+++ twodigitnumber.js
@@ -3,8 +3,9 @@ angular.module('filter.twodigitnumber', [])
     .filter('twodigitnumber', function () {
         return function (num) {
             num = num == undefined ? 0 : parseFloat(num);
-            if (num < 1) return num.toFixed(2).substr(1);
-            if (num < 10) return num.toFixed(1);
-            return num.toFixed(0);
+            if (num == 0) return '0';
+            if (num < 1) return num.toFixed(2).toString().substr(1);
+            if (num < 10) return num.toFixed(1).toString();
+            return num.toFixed(0).toString();
         }
     });
